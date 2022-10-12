883410ebdb0f8b529851917362d16c6198fbb23f
MicaelaTrujilloSoliz <201900826@est.umss.edu>
2022-10-12 17:56:29 -0400

    base de datos

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import Home from "./pages/Home";
 import FormularioPreInscripcion from "./pages/FormularioPreInscripcion";
 import FormularioRegistro from "./pages/FormularioRegistro";
 
+// import { doc, getDoc } from "firebase/firestore";
+// import { db } from "./Firebase/ConexionBD";
+
 /*import logo from './logo.svg';*/
 import './App.css';
 
 
 function App() {
+
+//  async function prueba(){
+//   const docRef = doc(db, "Campeonato1", "OKfiQOn7WhvKSck3A4Tf");
+//   const docSnap = await getDoc(docRef);
+  
+//   if (docSnap.exists()) {
+//     console.log("Document data:", docSnap.data());
+//   } else {
+//     // doc.data() will be undefined in this case
+//     console.log("No such document!");
+//   }
+//  }
+//  prueba();
+
   return (
     <BrowserRouter>
       <Routes>
