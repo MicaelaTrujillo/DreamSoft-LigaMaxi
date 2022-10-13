3cdf13d7020f9cdba4d527da9a4c51c6a7da96b2
MicaelaTrujilloSoliz <201900826@est.umss.edu>
2022-10-13 04:00:00 -0400

    union

--- src/components/FormPreinsc.js
+++ src/components/FormPreinsc.js
@@ -6,9 +6,9 @@ import { useState } from 'react';
 import css from "../styles sheet/FormPreinscripcion.css"
 
 import { db } from "../Firebase/ConexionBD";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc,getDocs } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
+import { collection} from "firebase/firestore";
 
 
 function FormPreinsc() {
@@ -30,14 +30,67 @@ function FormPreinsc() {
       
     async function onSubmit(e){
         e.preventDefault();
-        // Add a new document in collection "cities"
-        await setDoc(doc(db, "Campeonato1", "OKfiQOn7WhvKSck3A4Tf", "Solicitudes", nombre.campo), {
-        NombreEquipo: nombre.campo,
-        Categoria: categoria.campo,
-        });
-        console.log("esaqui", comprobante)
-        uploadFile(comprobante);
+        setTimeout(repetido,5000);
+        console.log("rep",controlar)
+        /*if(controlar === true){
+            // Add a new document in collection "cities"
+            await setDoc(doc(db, "Campeonato1", "OKfiQOn7WhvKSck3A4Tf", "Solicitudes", nombre.campo), {
+            NombreEquipo: nombre.campo,
+            Categoria: categoria.campo,
+            });
+            //console.log("esaqui", comprobante)
+            //uploadFile(comprobante);
+        }else{
+            console.log("hay repetidos")
+        }*/
     }
+
+
+
+    async function testTitulo() {
+    
+        //document.getElementById("botonReg").disabled = true;
+        var titulo = nombre.campo;
+        //Reemplazamos los saltos de linea por espacios
+        titulo = titulo.replace (/\r?\n/g," ");
+        //Reemplazamos los espacios seguidos por uno solo
+        titulo = titulo.replace (/[ ]+/g," ");
+        //Quitarmos los espacios del principio y del final
+        titulo = titulo.replace (/^ /,"");
+        titulo = titulo.replace (/ $/,"");
+        //comas
+        titulo = titulo.replace (/,,+/g,",");
+        
+        for (let j=0; j < titulos.length; j++) {
+            console.log(titulo,titulos[j])
+             if (titulo.toLowerCase() == titulos[j].toLowerCase()) {
+                 
+                 controlar = false;
+                 console.log("test", controlar)
+                 alert("El equipo ya está registrado en la base de datos.")
+                 j = titulos.length + 1;
+             }else{
+                await setDoc(doc(db, "Campeonato1", "OKfiQOn7WhvKSck3A4Tf", "Solicitudes", nombre.campo), {
+                    NombreEquipo: nombre.campo,
+                    Categoria: categoria.campo,
+                    });
+             }
+     
+         }
+    }
+    var controlar = true;
+    var titulos = []; 
+    async function repetido(){
+       
+        const querySnapshot = await getDocs(collection(db, "Campeonato1","OKfiQOn7WhvKSck3A4Tf","Solicitudes"));
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          titulos.push(doc.data().NombreEquipo)
+          console.log(titulos)
+          testTitulo();
+        });
+    }
+
     function uploadFile(file){
         const storageRef = ref(getStorage, 'Comprobantes/some-child')
         console.log("llega")
@@ -63,37 +116,41 @@ function FormPreinsc() {
     return (
     <>
     <div className="row cont-main-form mt-5 mb-5 mx-0">
-        <Form className="form text-center container col-8 ">
-            <h3 className="mb-5 mt-3">FORMULARIO DE PRE-INSCRIPCION</h3>
-                <FormInputs
-                    label="Nombre de equipo: "
-                    placeholder="Ingrese el nombre del equipo"
-                    estado={nombre}
-                    cambiarEstado={cambiarNombre} 
-                    expresionRegular = {expresiones.nombreEquipo}
-                />
-                <FormComboBox
-                    label="Categoría: "
-                    arreglo = {["30 años", "35 años", "40 años"]}
-                    estado={categoria}
-                    cambiarEstado={cambiarCategoria} 
-                />
-                <FormQR/>
-                <FormArchivo
-                    archivo="Subir comprobante:"
-                    estado={comprobante}
-                    cambiarEstado={cambiarComprobante}
-                />
-                <div className='botones pb-4'>
-                    <Boton 
-                        texto='Cancelar'/>
+        <div className='formulario'> 
+            <Form className="form text-center container col-8 ">
+                <h3 className="mb-5 mt-3">FORMULARIO DE PRE-INSCRIPCION</h3>
+                    <FormInputs
+                        label="Nombre de equipo: "
+                        placeholder="Ingrese el nombre del equipo"
+                        estado={nombre}
+                        cambiarEstado={cambiarNombre} 
+                        expresionRegular = {expresiones.nombreEquipo}
+                        alerta="El nombre del equipo no debe contener caracteres especiales"
+                        id="1"
+                    />
+                    <FormComboBox
+                        label="Categoría: "
+                        arreglo = {["30 años", "35 años", "40 años"]}
+                        estado={categoria}
+                        cambiarEstado={cambiarCategoria} 
+                    />
+                    <FormQR/>
+                    <FormArchivo
+                        archivo="Subir comprobante:"
+                        estado={comprobante}
+                        cambiarEstado={cambiarComprobante}
+                    />
+                    <div className='botones pb-4'>
+                        <Boton 
+                            texto='Cancelar'/>
 
-                    <Boton 
-                        texto='Enviar'
-                        manejarClic={onSubmit}
-                        />
-            </div>
-        </Form>
+                        <Boton 
+                            texto='Enviar'
+                            manejarClic={onSubmit}
+                            />
+                </div>
+            </Form>
+        </div>
     </div>
     </>
   );
