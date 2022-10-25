7160633e6d4316f8a6cfda2a23b9e6860893ddab
MicaelaTrujilloSoliz <201900826@est.umss.edu>
2022-10-24 20:14:29 -0400

    inicio sprint2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FormularioPreInscripcion from "./pages/FormularioPreInscripcion";
 import FormularioRegistro from "./pages/FormularioRegistro";
+import FormularioCampeonato from "./pages/FormularioCampeonato";
+import FormularioInscripcion from "./pages/FormularioInscripcion";
+
 import './App.css';
 import { app } from "./Firebase/ConexionBD";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -17,6 +20,8 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/FormularioPreInscripcion" element={<FormularioPreInscripcion/>}></Route>
           <Route path="/FormularioRegistro" element={<FormularioRegistro/>}></Route>
+          <Route path="/FormularioCampeonato" element={<FormularioCampeonato/>}></Route>
+          <Route path="/FormularioInscripcion" element={<FormularioInscripcion/>}></Route>
         </Routes>
     </UserProvider>
   );
---
+++ src/components/FormInscripcion.js
@@ -0,0 +1,55 @@
+
+import {FormInputSinCambioEst, Boton, LabelForm} from '../Elementos/ElementosForms'
+import Form from "react-bootstrap/Form";
+
+
+function FormInscripcion() {
+    return (
+        <div className="row cont-main-form mt-2 mb-2 mx-0">
+            
+                <Form className="form text-center container col-8 ">
+                    <h3 className="mb-5 mt-3">FORMULARIO DE INSCRIPCIÓN</h3>
+                        <FormInputSinCambioEst
+                            label="Nombre del equipo: "
+                            value = "Jaguares"
+                             
+                        />
+                        <FormInputSinCambioEst
+                            label="Categoría: "
+                            value = "35 años"
+                             
+                        />
+                        <LabelForm
+                            label="Añadir jugador: "
+                        />
+                        <div className='botones pb-4'>
+                            
+                            <Boton
+                                    texto='Añadir'      
+                            />
+                        </div>
+
+
+                        <LabelForm
+                                label="Añadir jugador: "
+                            />
+                        <div className='botones pb-4'>
+                            
+                            <Boton
+                                    texto='Añadir'      
+                            />
+                        </div>
+
+                        <div className='botones pb-4'>
+                            <Boton 
+                                texto='Cancelar'/>
+    
+                            <Boton 
+                                texto='Registrar'
+                                />
+                        </div>
+                </Form>
+        </div>
+      );
+}
+export default FormInscripcion;
