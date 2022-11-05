cd9181ebb1850a3b7f2e1503d4c2af065414942e
aleydacesp <201900815@est.umss.edu>
2022-11-05 00:17:38 -0400

    obtencion de grupos

--- src/components/ContenedorSol.js
+++ src/components/ContenedorSol.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "../styles sheet/ContenedorSol.css"
 import TarjetaSol from "./TarjetaSol";
+import { db } from "../Firebase/ConexionBD";
+import { doc, setDoc,getDocs,  collection, getDoc, updateDoc} from "firebase/firestore";
+import { Button } from "bootstrap";
+import { async } from "@firebase/util";
 
 function ContenedorSol(){
+    const equipos =[];
+    async function obtenerEquipos(){
+        const querySnapshot = await getDocs(collection(db, "Campeonato1","OKfiQOn7WhvKSck3A4Tf","Solicitudes"));
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          equipos.push(doc.data().NombreEquipo)
+          console.log("Matriz de equipos", equipos);
+        });
+    }
     return(
         <div className="main">
             <div className="contenedor-sol">
@@ -11,6 +24,7 @@ function ContenedorSol(){
                 <TarjetaSol/>
                 <TarjetaSol/>
                 <TarjetaSol/>
+                <button onClick={obtenerEquipos}>ver equipos</button>
             </div>
         </div>
     );
